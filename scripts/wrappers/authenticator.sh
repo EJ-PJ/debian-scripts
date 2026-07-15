7aa89d8bfeb98766c5964d64acd1554e0a3a82fd
eylles <ed.ylles1997@gmail.com>
2026-07-15 12:52:50 -0600

    Make authenticator.sh configurable

--- scripts/wrappers/authenticator.sh
+++ scripts/wrappers/authenticator.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
+
 name=Authenticator
-x-terminal-emulator -g 118x32 -T "$name" -e cotp
+totp_prog=cotp
+
+config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/totp-authenticator"
+config_file="${config_dir}/configrc"
+
+if [ -r "$config_file" ]; then
+    . "$config_file"
+else
+    if [ ! -d "$config_dir" ]; then
+        mkdir -p "$config_dir"
+    fi
+    cat << __HEREDOC__ > "$config_file"
+# vim: ft=sh
+# authenticator config file
+
+# name
+name="${name}"
+
+# program to launch
+totp_prog="$totp_prog"
+__HEREDOC__
+fi
+
+
+x-terminal-emulator -g 118x32 -T "$name" -e "$totp_prog"
