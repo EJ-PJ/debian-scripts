89bf233f7c1df36e8280beff79b50ef1102661e4
eylles <ed.ylles1997@gmail.com>
2026-07-17 17:56:26 -0600

    Add option to run topt program without a terminal
    If the wrapper script is used to run any totp program that is not cotp be it that it already does
    handling to run within a terminal or is an actual gui program that needs no terminal emulator to run

--- scripts/wrappers/authenticator.sh
+++ scripts/wrappers/authenticator.sh
@@ -5,6 +5,8 @@ totp_prog=cotp-wrap
 
 term_geom="118x32"
 
+run_without_terminal=""
+
 config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/totp-authenticator"
 config_file="${config_dir}/configrc"
 
@@ -26,8 +28,17 @@ totp_prog="$totp_prog"
 
 # terminal window geometry
 term_geom="$term_geom"
+
+# werether to run the desired totp without a terminal
+# setting to a value different from empty will just run
+# the totp program as is, otherwise x-terminal-emulator is used
+run_without_terminal="$run_without_terminal"
 __HEREDOC__
 fi
 
 
-exec x-terminal-emulator -g "$term_geom" -T "$name" -e "$totp_prog"
+if [ -z "$run_without_terminal" ]; then
+    exec x-terminal-emulator -g "$term_geom" -T "$name" -e "$totp_prog"
+else
+    exec "$totp_prog"
+fi
